88a9588e66f7cc18f8803592f6d6bac650ff19e0
Faizan Afzal <thefaizan@gmail.com>
2019-03-08 12:48:50 +0500

    added the missing migrations

---
+++ src/Migrations/2019_01_01_142407_create_fb_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFbSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fbinsights_settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('meta_key')->unique();
+            $table->longText('page_insights')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fbinsights_settings');
+    }
+}
---
+++ src/Migrations/2019_01_03_212137_create_fb_page_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFbPagePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fbinsights_page_posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('page_id')->nullable();             
+            $table->string('post_id')->nullable();             
+            $table->longText('post_insights')->nullable();            
+            $table->timestamps();
+
+            $table->index(['user_id', 'page_id', 'post_id']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fbinsights_page_posts');
+    }
+}
